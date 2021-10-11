643b2a43f7f8fb08cfa1773c5b8dd7d6efb2ebee
Leonor Drummond <84380184+ltadrummond@users.noreply.github.com>
2021-10-11 10:17:11 +0200

    add new directories'

---
+++ deployment/app.py
@@ -0,0 +1,9 @@
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image, ImageOps
+from img_classification import *
+from tensorflow import keras
